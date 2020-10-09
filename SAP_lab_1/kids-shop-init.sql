f19a96925453f085650a05fa92074fdfab5b3549
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-09 16:06:48 +0600

    SAP

---
+++ SAP_lab_1/kids-shop-init.sql
@@ -0,0 +1,42 @@
+/*
+-- CLEAN UP
+-- run the following lines to clean up the database
+drop table category;
+drop table product;
+drop table vote;
+*/
+
+/*
+If you already have the database, do not run the next two lines
+*/
+create database kids_shop;
+use kids_shop;
+
+create table category (
+    id int not null auto_increment primary key,
+    name varchar(100)
+);
+
+create table product (
+    id int not null auto_increment primary key,
+    name varchar(100),
+    category_id int not null,
+    votes int not null default 0
+);
+
+create table vote (
+    id int not null auto_increment primary key,
+    product_id int not null,
+    is_up_vote bit
+);
+
+delimiter //
+create procedure recalculate_product_votes()
+begin
+    update product p
+        set votes =
+            (select count(*) from vote where product_id = p.id and is_up_vote = true)
+            - (select count(*) from vote where product_id = p.id and is_up_vote = false)
+    where 1 = 1;
+end;//
+delimiter ;
---
+++ SAP_lab_1/kids-shop-seed.sql
@@ -0,0 +1,29 @@
+use kids_shop;
+
+insert into category (name) values
+    ('toys'),
+    ('cloths'),
+    ('diaper');
+
+insert into product (name, category_id) values
+    ('light ball', 1),
+    ('blocks', 1),
+    ('cool red shirt', 2),
+    ('long blue skirt', 2),
+    ('nice green pants', 2),
+    ('kids comfi', 3);
+
+insert into vote(product_id, is_up_vote) values
+    (1, true),
+    (1, true),
+    (1, false),
+    (2, false),
+    (2, false),
+    (3, true),
+    (3, true),
+    (5, false),
+    (5, true),
+    (5, true),
+    (5, true);
+
+call recalculate_product_votes();
