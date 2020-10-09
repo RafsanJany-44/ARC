2196adbbd959a257ab0aa68373f4590fd8069844
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-09 13:06:15 +0600

    Update UnitTest.cs

--- ConfMatrix/ConfMatrix/UnitTest.cs
+++ ConfMatrix/ConfMatrix/UnitTest.cs
@@ -3,15 +3,35 @@ using Xunit;
 public class TestClass{
    Calc obj= new Calc("72","9","4","30");
    // acurate result what we are seeking for:
-   float expectedAccuracy = 0.66086954;
-    float expectedPrecision = 0.8888889;
-    float expectedSensitivity = 0.7058824;
-    float expectedSpecificity = 0.30769232;
-    float expectedF1Score = 0.78688526;
+   float expectedAccuracy = 0.66086954f;
+    float expectedPrecision = 0.8888889f;
+    float expectedSensitivity = 0.7058824f;
+    float expectedSpecificity = 0.30769232f;
+    float expectedF1Score = 0.78688526f;
 
     [Fact]
     public void TestAccuracy()
     {
         Assert.Equal(expectedAccuracy,obj.accuracy());
+    }
+    [Fact]
+    public void TestPrecision()
+    {
+        Assert.Equal(expectedPrecision,obj.precision());
+    }
+    [Fact]
+    public void TestSensitivity()
+    {
+        Assert.Equal(expectedSensitivity,obj.sensitivity());
+    }
+    [Fact]
+    public void TestSpecificity()
+    {
+        Assert.Equal(expectedSpecificity,obj.specificity());
+    }
+    [Fact]
+    public void TestF1Score()
+    {
+        Assert.Equal(expectedF1Score,obj.f1Score());
     }
 }
