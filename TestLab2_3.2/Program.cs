d037048c62c8c427a29e49d8d1d88f83579ae583
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-14 11:50:14 +0600

    lab2 created for practice

---
+++ TestLab2_3.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLab2_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
