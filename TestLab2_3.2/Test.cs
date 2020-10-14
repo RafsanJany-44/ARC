18c3d67ee6b25f9f4ff5e8b90b8179e5825d4aa5
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-14 23:07:44 +0600

    #loop added

--- TestLab2_3.2/Test.cs
+++ TestLab2_3.2/Test.cs
@@ -16,7 +16,13 @@ public class Test{
 
     [Fact]
     public void test3(){
+        
         Assert.Equal(5,TestLab2_3._2.Program.vod_return(3,4));
+    }
+    [Fact]
+    public void test4(){
+        for(int i=-1;i<=10;i=i+1)
+        Assert.Equal(i,TestLab2_3._2.Program.findSum(4));
     }
 
 }
