2f0118cc3fe5ec30e677eacf03e24d99825b5788
Rafsan Jany, 170042044 <rafsanjany@iut-dhaka.edu>
2020-10-15 12:23:59 +0600

    updates

--- privateUniversity/qsn3.c
+++ privateUniversity/qsn3.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 int main(){
-    int i, j,k,n,m;
+    int i, j,k,n;
     char c1,c2;
 
     scanf("%d",&n);
-    m=n+1;
     printf("Character 1:");
     scanf(" %c",&c1);
     printf("Character 2:");
     scanf(" %c",&c2);
     printf("\n");
 
-    for(i=1; i<=m-1; i++)
+    for(i=1; i<=n; i++)
     {
-        for(j=i; j<m; j++)
+        for(j=i; j<n+1; j++)
         {
             printf(" ");
         }
         for(j=1; j<=(2*i-1); j++)
         {
 
-            if(i==m || j==1 || j==(2*i-1))
+            if(i==n+1 || j==1 || j==(2*i-1))
             {
                 printf("%c",c1);
             }
@@ -32,7 +31,7 @@ int main(){
         printf("\n");
     }
     printf("  ");
-    for(k=0;k<=m-1;k++){
+    for(k=0;k<=n;k++){
     printf("%c",c2);
     }
 }
